0de3c1c9263db9d7f77c8618a36ce323337e3713
Shawn Throop <shawnthroop@gmail.com>
2013-09-23 15:29:04 -0400

    Initial Commit

---
+++ TableViewTest/TVTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVTAppDelegate.h
+//  TableViewTest
+//
+//  Created by Shawn Throop on 2013-09-23.
+//  Copyright (c) 2013 Silent H Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewTest
+//
+//  Created by Shawn Throop on 2013-09-23.
+//  Copyright (c) 2013 Silent H Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVTAppDelegate class]));
+    }
+}
